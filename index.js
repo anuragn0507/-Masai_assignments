273ebb6b7d18937be416db75d186cff0b4524948
Anurag <anuragn1107@gmail.com>
2022-09-12 15:19:23 +0530

    todo list saving the data in localstorage

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
+var arr = JSON.parse(localStorage.getItem("todos"))||[]
 
-var todoData = JSON.parse(localStorage.getItem("todo")) ||[];
-console.log("local storage ka data", todoData);
-showData(todoData);
+
+// console.log("local storage ka data", todoData);
+showData(arr);
 
 document.querySelector("form").addEventListener("submit",myTodo)
-function myTodo(event){    
-    var todoData = JSON.parse(localStorage.getItem("todo")) ||[];
-    var arr =[];
-    event.preventDefault();
 
-    var task = document.getElementById("task").value;
-    var priority = document.getElementById("priority").value;
-
-    todoList ={
-        task:task,
-        priority:priority,
-    }
+function myTodo(event){
+    event.preventDefault();var task1 = document.getElementById("task").value;
+    var priority1 = document.getElementById("priority").value;
     
-    if(todoData.length ==0){
+    var todoList = {
+        task: task1,
+        priority: priority1,
+    }
+    console.log("todo me kya jaa rha hai?", arr);
         arr.push(todoList);
-        localStorage.setItem("todo", JSON.stringify(arr));
-        var todoData = JSON.parse(localStorage.getItem("todo")) ||[];
-        showData(todoData);
-    }else{
-        todoData.push(todoList);
-        showData(todoData);
-    }  
-    console.log("todo me kya jaa rha hai?", todoData);
-     
+        localStorage.setItem("todos", JSON.stringify(arr));
        
-}
+        showData(arr);
+    }
 
 
-
-function deleteRow(){
-    // console.log(event.target.parentNode);
+function deleteRow(index){
+    // console.log(event.target.parentNode);    
+    var arr = JSON.parse(localStorage.getItem("todos"));
+    console.log("index" , index);
+    arr.splice(index,1);
     event.target.parentNode.remove();
+    localStorage.setItem("todos", JSON.stringify(arr));
+    // showData(arr);
+
 }
 
 function showData(todoData){
+    var body = document.getElementById("body");
+    //most important part.
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+      }
+
     todoData.map(function(elem, index){
         var tr = document.createElement("tr");
         var td1 = document.createElement("td");
@@ -58,7 +59,9 @@ function showData(todoData){
         td3.innerText = "Delete"
     
         td3.style.color = "crimson"
-        td3.addEventListener("click", deleteRow);
+        td3.addEventListener("click", function(){
+            deleteRow(index);
+        });
     
         tr.append(td1, td2, td3);
         document.querySelector("tbody").append(tr); 
