cc6e9367d6b6cd577ead90d96e36e78f6e490de5
Anurag <anuragn1107@gmail.com>
2022-10-03 03:55:45 +0530

    Movie app practice

---
+++ MovieApp/scripts/index.js
@@ -0,0 +1,11 @@
+// Store the wallet amount to your local storage with key "amount"
+
+document.getElementById("add_to_wallet").addEventListener("click",addToWallet)
+
+function addToWallet(){
+    let amount=  +document.getElementById("amount").value;
+    console.log(amount);
+    let total = +JSON.parse(localStorage.getItem("amount"));
+    total += amount;
+    localStorage.setItem("amount", total);
+}
